fe6f71c6bb40253eeb355d1df735c9032fecb221
Nicolas Mérouze <nicolas.merouze@gmail.com>
2015-07-15 11:14:04 +0900

    Add test for the required arg.

--- test/markdown.js
+++ test/markdown.js
@@ -2,18 +2,28 @@ var assert = require('assert');
 var test = require('mocha').test;
 var markdown = require('..');
 
-test('.toHtml', function(done) {
-  var testCases = [
-    {
-      args: { markdown: '# Hello', output: 'markdown' },
-      output: '<h1 id="hello">Hello</h1>\n'
-    }
-  ];
+var testCases = [
+  {
+    name: 'toHtml',
+    args: { markdown: '# Hello', output: 'markdown' },
+    output: '<h1 id="hello">Hello</h1>\n'
+  },
+  {
+    name: 'toHtml#noargs',
+    args: {},
+    output: 'error'
+  }
+];
 
-  testCases.forEach(function(testCase) {
+testCases.forEach(function(testCase) {
+  test(testCase.name, function(done) {
     markdown.toHtml({}, testCase.args).then(function(ctx) {
       assert.equal(ctx.markdown, testCase.output);
       done();
+    }).catch(function() {
+      if (testCase.output === 'error') {
+        done();
+      }
     });
   });
 });
